8e8ac59df5f0c280822ef01146d58adff33f4c59
cesslab <anwarruff@gmail.com>
2017-08-04 12:35:56 -0400

    Added handlers for displaying calibration screens

--- src/main/java/edu/nyu/cess/controller/CalibrationController.java
+++ src/main/java/edu/nyu/cess/controller/CalibrationController.java
@@ -1,10 +1,14 @@
 package edu.nyu.cess.controller;
 
-import edu.nyu.cess.concurrent.CalibrationService;
+import com.jfoenix.controls.JFXButton;
+import edu.nyu.cess.net.CalibrationTask;
+import edu.nyu.cess.net.ShowCalibrationScreenTask;
 import edu.nyu.cess.scene.SceneName;
+import javafx.application.Platform;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -12,32 +16,88 @@ import java.util.ResourceBundle;
 
 public class CalibrationController implements Updatable, Initializable {
     @FXML
-    Button calibrateButton;
-    @FXML
-    Label statusLabel;
+    JFXButton displayScreenButton;
 
-    private CalibrationService service;
+    @FXML
+    JFXButton startCalibrationButton;
+    @FXML
+    JFXButton nextButton;
+
+    @FXML
+    Label feedbackLabel;
+
+    private Service showScreenService;
+    private Service calibrationService;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-    //    service = new CalibrationService();
-    //    statusLabel.textProperty().bind(service.messageProperty());
-    }
-
-    @FXML
-    public void calibrateButtonClicked() {
-//        if (service.getState() == Service.State.SUCCEEDED) {
-//            service.reset();
-//            service.start();
-//        }
-//        else if (service.getState() == Service.State.READY) {
-//            service.start();
-//        }
-//        System.out.println(service.getState().toString());
     }
 
     @Override
     public void onNavigateUpdate(SceneName previousSceneName) {
+        if (showScreenService == null) {
+            Task<Boolean> showScreenTask = new ShowCalibrationScreenTask();
+            showScreenTask.setOnSucceeded(e -> Platform.runLater(this::screenDisplayed));
+            showScreenTask.setOnFailed(e -> Platform.runLater(this::displayScreenFailed));
+            showScreenService = new Service() {
+                @Override
+                protected Task<Boolean> createTask() {
+                    return showScreenTask;
+                }
+            };
+        }
+
+        if (calibrationService == null) {
+            Task<Boolean> calibrationTask = new CalibrationTask();
+            calibrationTask.setOnSucceeded(e -> Platform.runLater(this::calibrationSucceeded));
+            calibrationTask.setOnFailed(e -> Platform.runLater(this::calibrationFailed));
+
+            calibrationService = new Service() {
+                @Override
+                protected Task<Boolean> createTask() {
+                    return calibrationTask;
+                }
+            };
+        }
+    }
+
+    private void handleServiceRequest(Service service) {
+        if (service.getState() == Service.State.SUCCEEDED) {
+            service.reset();
+            service.start();
+        }
+        else if (service.getState() == Service.State.READY) {
+            service.start();
+        }
+        System.out.println(service.getState().toString());
 
     }
+
+    @FXML
+    public void displayScreenClicked() {
+        handleServiceRequest(showScreenService);
+    }
+
+    public void screenDisplayed() {
+        feedbackLabel.setText("Calibration Screen Displayed");
+    }
+
+    public void calibrationSucceeded() {
+
+    }
+
+    public void calibrationFailed() {
+
+    }
+
+    @FXML
+    public void calibrateButtonClicked() {
+        handleServiceRequest(calibrationService);
+    }
+
+    @FXML
+    public void displayScreenFailed() {
+        feedbackLabel.setText("Failed to Display Calibration Screen");
+    }
+
 }
