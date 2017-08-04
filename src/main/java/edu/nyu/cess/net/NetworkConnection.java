37e202f14b243f843af5f960428460fb89bd3947
cesslab <anwarruff@gmail.com>
2017-08-04 12:11:50 -0400

    Added buffered reader, and print writer.

--- src/main/java/edu/nyu/cess/net/NetworkConnection.java
+++ src/main/java/edu/nyu/cess/net/NetworkConnection.java
@@ -3,34 +3,48 @@ package edu.nyu.cess.net;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class NetworkConnection {
     private static Socket socket;
-    private static boolean connected;
     private static SocketConnectionTask socketConnectionTask;
 
     public static boolean isConnected() {
-        if (socket != null) {
-            return socket.isConnected();
-        }
-        return false;
+        return socket != null && socket.isConnected();
     }
 
     public static void connect(String ipAddress, int port,
                                EventHandler<WorkerStateEvent> onConnect, EventHandler<WorkerStateEvent> onFail) {
         socketConnectionTask = new SocketConnectionTask(ipAddress, port);
-        socketConnectionTask.setOnFailed(e -> {
-            connected =  false;
-            onFail.handle(e);
-        });
+        socketConnectionTask.setOnFailed(onFail);
 
         socketConnectionTask.setOnSucceeded(e -> {
-            connected = true;
             socket =  socketConnectionTask.getValue();
             onConnect.handle(e);
         });
         new Thread(socketConnectionTask).start();
+    }
+
+    public static PrintWriter getPrintWriter() {
+        try {
+            return new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static BufferedReader getBufferedReader() {
+        try {
+            return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void cancelConnect() {
