f7d0a551d146fe21e8f3e5769c2ef9aa6c02cc96
cesslab <anwarruff@gmail.com>
2017-08-04 12:02:58 -0400

    Updated calibration show id and message name

--- src/main/java/edu/nyu/cess/gazepoint/Calibrate.java
+++ src/main/java/edu/nyu/cess/gazepoint/Calibrate.java
@@ -2,9 +2,9 @@ package edu.nyu.cess.gazepoint;
 
 
 public class Calibrate {
-    public static final String SHOW_ID = "CALIBRATE_SHOW";
+    public static final String SHOW_SCREEN_ID = "CALIBRATE_SHOW";
     public static final String START_ID = "CALIBRATE_START";
     public static final String CALIBRATION_RESULT_ID = "CALIB_RESULT";
-    public static final String SHOW_MESSAGE = "<SET ID=\"CALIBRATE_SHOW\" STATE=\"1\" />\r\n";
+    public static final String SHOW_SCREEN_MESSAGE = "<SET ID=\"CALIBRATE_SHOW\" STATE=\"1\" />\r\n";
     public static final String START_MESSAGE = "<SET ID=\"CALIBRATE_START\" STATE=\"1\" />\r\n";
 }
